f7403f3b71df00141e0c0cab3868d83004204705
Eastvantage <eastvantage@ev.local>
2018-08-23 16:58:05 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tuesday-am';
+  title = 'Tour of Heroes';
 }
